09235dcc2ae7019beb0b561ad4804998c273c466
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-07-16 13:59:30 +0200

    Refactor LynisReport

--- src/compleasy.py
+++ src/compleasy.py
@@ -17,40 +17,46 @@ DATABASE = APPDIR + '/compleasy.db'
 class LynisReport:
     def __init__(self, report_file_or_content):
         self.keys = {}
-        if os.path.exists(report_file_or_content):
-            with open(report_file_or_content, 'r') as file:
-                self.report = file.read()
-        else:
-            self.report = report_file_or_content
+        self.report = self.read_report(report_file_or_content)
         self.keys = self.parse_report()
 
+    def read_report(self, report_file_or_content):
+        """Read the report from a file or direct content."""
+        if os.path.exists(report_file_or_content):
+            with open(report_file_or_content, 'r') as file:
+                return file.read()
+        return report_file_or_content
+
     def get_full_report(self):
+        """Return the full report content."""
         return self.report
 
     def parse_report(self):
+        """Parse the report and count warnings and suggestions."""
+        warning_count = 0
+        suggestion_count = 0
+        parsed_keys = {}
+        
         for line in self.report.split('\n'):
-            if not line:
+            if not line or line.startswith('#') or '=' not in line:
                 continue
-            if line.startswith('#'):
-                continue
-            if '=' not in line:
-                continue
-            # If there are multiple '=' in the line, split only the first one
+            
             key, value = line.split('=', 1)
-            self.keys[key] = value
-        # Add warnings and suggestions keys
-        self.keys['count_warnings'] = self.count_warnings() or 0
-        self.keys['count_suggestions'] = self.count_suggestions() or 0
-        return dict(self.keys)
+            if key == 'warning[]':
+                warning_count += 1
+            elif key == 'suggestion[]':
+                suggestion_count += 1
+            
+            parsed_keys[key] = value
+
+        parsed_keys['count_warnings'] = warning_count
+        parsed_keys['count_suggestions'] = suggestion_count
+        
+        return parsed_keys
     
     def get(self, key):
+        """Get the value of a specific key."""
         return self.keys.get(key)
-    
-    def count_warnings(self):
-        return len([k for k in self.keys.keys() if k.startswith('warning[]')])
-    
-    def count_suggestions(self):
-        return len([k for k in self.keys.keys() if k.startswith('suggestion[]')])
 
 ########################################################################################
 # Database functions
