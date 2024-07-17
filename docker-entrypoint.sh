3f0ca4440cfb83898de3bab4c064b9a8f0f665b2
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-07-17 14:13:09 +0200

    chore: Update admin user password handling and entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,16 +3,19 @@
 set -e
 
 # COMPLEASY_USERNAME
-COMPLEASY_ADMIN_USERNAME=${COMPLEASY_USERNAME:-admin}
+COMPLEASY_ADMIN_USERNAME=${COMPLEASY_ADMIN_USERNAME:-admin}
 # COMPLEASY_PASSWORD
-COMPLEASY_ADMIN_PASSWORD=${COMPLEASY_PASSWORD:-admin}
-DJANGO_SUPERUSER_PASSWORD=${COMPLEASY_PASSWORD}
+COMPLEASY_ADMIN_PASSWORD=${COMPLEASY_ADMIN_PASSWORD:-admin}
+DJANGO_SUPERUSER_PASSWORD=${COMPLEASY_ADMIN_PASSWORD}
 
 # Apply database migrations
 python manage.py makemigrations
 
-# Create admin user
-python manage.py createsuperuser --noinput --username=admin
+# Create admin user (ignore errors)
+python manage.py createsuperuser --noinput --username=admin || true
+
+# Update admin user password (from environment variable)
+python manage.py change_admin_password
 
 # Add a random licensekey
 python manage.py populate_db_licensekey
---
+++ src/api/management/commands/change_admin_password.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+class Command(BaseCommand):
+    help = 'Change the password of the admin user'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--password', help='New password for the admin user', required=False)
+
+    def handle(self, *args, **options):
+        # Get the admin user
+        admin_user = User.objects.get(username='admin')
+
+        if not admin_user:
+            self.stdout.write(self.style.ERROR('Admin user not found'))
+            return
+        
+        # Get new password from environment variable
+        new_password = os.environ.get('COMPLEASY_ADMIN_PASSWORD')
+
+        # Get the new password from the command line
+        if options['password']:
+            new_password = options['password']
+
+        # Change the password of the admin user
+        admin_user.set_password(new_password)
+        admin_user.save()
