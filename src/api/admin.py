3d2c66bf8af16f4b31a2e667a7249d9e431f7b99
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-07-24 20:13:14 +0200

    feat: Add DiffReport and DiffReportAdmin models
    Added a new model called `DiffReport` to store device diff reports. The `DiffReport` model includes fields for `device`, `diff_report`, and `created_at`. This allows for storing and retrieving device diff reports in the application.
    
    Also added the `DiffReportAdmin` model admin class to customize the display of `DiffReport` objects in the admin interface.

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import LicenseKey, Device, FullReport
+from .models import LicenseKey, Device, FullReport, DiffReport
 
 @admin.register(LicenseKey)
 class LicenseKeyAdmin(admin.ModelAdmin):
@@ -8,8 +8,14 @@ class LicenseKeyAdmin(admin.ModelAdmin):
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('hostid', 'hostid2', 'hostname', 'os', 'distro', 'distro_version', 'lynis_version', 'last_update', 'warnings')
+    list_display = ('hostname', 'licensekey', 'created_at', 'updated_at', 'warnings')
+
 
 @admin.register(FullReport)
 class FullReportAdmin(admin.ModelAdmin):
     list_display = ('device', 'full_report', 'created_at')
+
+
+@admin.register(DiffReport)
+class DiffReportAdmin(admin.ModelAdmin):
+    list_display = ('device', 'diff_report', 'created_at')
