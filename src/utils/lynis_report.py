ec6344e34c2d5e3a1808893cee28ef047a7bae07
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-07-24 20:15:48 +0200

    feat: Refactor LynisReport class to compare and store differences in reports
    The `LynisReport` class in `lynis_report.py` has been refactored to include a new method `diff` that compares two reports and returns the differences. The method ignores certain keys that are not relevant for the comparison and handles lists differently. The differences are stored in a dictionary with keys for added, removed, and changed values.
    
    This change improves the functionality of the `LynisReport` class by allowing for easy comparison and tracking of differences between reports.

--- src/utils/lynis_report.py
+++ src/utils/lynis_report.py
@@ -1,17 +1,11 @@
 import os
+import logging
 
 class LynisReport:
-    def __init__(self, report_file_or_content):
+    def __init__(self, full_report):
         self.keys = {}
-        self.report = self.read_report(report_file_or_content)
+        self.report = full_report
         self.keys = self.parse_report()
-
-    def read_report(self, report_file_or_content):
-        """Read the report from a file or direct content."""
-        if os.path.exists(report_file_or_content):
-            with open(report_file_or_content, 'r') as file:
-                return file.read()
-        return report_file_or_content
 
     def get_full_report(self):
         """Return the full report content."""
@@ -45,3 +39,50 @@ class LynisReport:
     def get(self, key):
         """Get the value of a specific key."""
         return self.keys.get(key)
+    
+    def diff(self, other_report):
+        """
+        Compare two reports and return the differences.
+        Ignore some keys that are not relevant for the comparison.
+        Take into account some keys are lists, so we need to compare them differently.
+        The dict contains a key "change" with the following values: added, removed, changed
+        """
+        keys_to_ignore = ['report_datetime_start', 'report_datetime_end', 'slow_test', 'uptime_in_seconds','count_warnings', 'count_suggestions']
+
+        diff = {
+            'added': {},
+            'removed': {},
+            'changed': {}
+        }
+
+        for key, value in self.keys.items():
+            # Ignore some keys
+            if key in keys_to_ignore:
+                continue
+
+            # if the key is a list, compare the lists and store only the differences
+            if isinstance(value, list):
+                if key in other_report.keys and value != other_report.keys[key]:
+                    # Compare the lists and store the differences. Avoid empty values
+                    added_values = list(set(value) - set(other_report.keys[key]))
+                    if added_values:
+                        diff['added'][key] = added_values
+                    
+                    removed_values = list(set(other_report.keys[key]) - set(value))
+                    if removed_values:
+                        diff['removed'][key] = removed_values
+
+            else:
+                if key in other_report.keys and value != other_report.keys[key]:
+                    # IF the value has | as separator, split it and store only the differences
+                    if '|' in value:
+                        # Split the value and compare the lists. Avoid empty values
+                        diff['changed'][key] = list(set(value.split('|')) - set(other_report.keys[key].split('|')))
+                    else:
+                        diff['changed'][key] = value
+                elif key not in other_report.keys:
+                    diff['removed'][key] = value
+                elif key not in self.keys:
+                    diff['added'][key] = value
+                    
+        return diff
