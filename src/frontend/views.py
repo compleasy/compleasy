2df3ae493dbad266856baa807951efe3fca39ad2
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-08-03 14:18:43 +0200

    refactor: Remove noisy 'installed_packages_array' key from diff analysis

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -192,21 +192,25 @@ def activity(request):
             break
         
         diff = diff_report.diff_report
+        
+        # Ignore some noisy or irrelevant keys
         ignore_keys = [
-            'report_datetime_start',    # It's always different
-            'report_datetime_end',      # It's always different
-            'slow_test',                # It will repeat the same line every audit
-            'uptime_in_seconds',        # It's always different
-            'uptime_in_days',           # It's always different
-            'deleted_file',             # Very noisy, is not relevant for the user
-            'lynis_timer_next_trigger', # It's always different
-            'clamav_last_update',       # It's always different (or should be)
-            'tests_executed',           # Fix this
-            'tests_skipped',            # Fix this
-            'installed_packages_array'  # Very noisy
+            'report_datetime_start',
+            'report_datetime_end',
+            'slow_test',
+            'uptime_in_seconds',
+            'uptime_in_days',
+            'deleted_file',
+            'lynis_timer_next_trigger',
+            'clamav_last_update',
+            'tests_executed',
+            'tests_skipped',
+            'installed_packages_array'
+            'vulnerable_package'
         ]
-        #diff_analysis = analyze_diff(diff, ignore_keys=ignore_keys)
+
         lynis_diff = LynisReport.Diff(diff)
+        # Generate a LynisReport object with the diff
         diff_analysis = lynis_diff.analyze(ignore_keys)
 
         # Check if 'added' and 'removed' keys are in the diff_analysis
